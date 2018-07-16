4c9154c65fb809e8563e08f360d7e06805ef0895
thol-voleak <voleak.rupp@gmail.com>
2018-07-16 09:31:41 +0700

    fixed bug

--- Jenkinsfile-Cron-Http.groovy
+++ Jenkinsfile-Cron-Http.groovy
@@ -42,7 +42,7 @@ pipeline {
             steps{
                 script{
                     invokerHttp()
-                    build("Test-Jmeter")
+                    //build("Test-Jmeter")
                 }
             }
         }
